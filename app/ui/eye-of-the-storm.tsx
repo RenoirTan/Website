98b264cc4fe57ef3dc4dd6503aa7ba62f2f3dce7
RenoirTan <renoirtan2005@gmail.com>
2024-02-01 15:16:14 +0800

    main: eye of the storm

---
+++ app/ui/eye-of-the-storm.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { clsx } from "clsx";
+
+export default function EyeOfTheStorm() {
+  // https://www.kindacode.com/article/react-get-the-position-x-y-of-an-element/
+  const imgRef = useRef<HTMLDivElement>(null);
+
+  const [hovering, setHovering] = useState(false);
+  const [x, setX] = useState(0); // x coords of origin
+  const [y, setY] = useState(0); // y coords of origin
+  const [width, setWidth] = useState(1); // width of image, including padding
+  const [height, setHeight] = useState(1); // height of image
+
+  const getDimensions = () => {
+    const topLeftX = imgRef.current?.offsetLeft || 0;
+    const topLeftY = imgRef.current?.offsetTop || 0;
+    const newWidth = imgRef.current?.offsetWidth || 1;
+    const newHeight = imgRef.current?.offsetHeight || 1;
+    const newX = topLeftX + (newWidth / 2);
+    const newY = topLeftY + (newHeight / 2);
+    setX(newX);
+    setY(newY);
+    setWidth(newWidth);
+    setHeight(newHeight);
+  }
+
+  // on load
+  useEffect(() => {
+    getDimensions();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", getDimensions);
+  }, []);
+
+  return (
+    <div ref={imgRef} className="scale-75">
+      <Image
+        className={clsx(
+          "duration-300 rounded-full shadow-xl shadow-orange-800",
+          hovering && "-rotate-12 md:-rotate-45 shadow-rose-500"
+        )}
+        src="/icon.png"
+        alt="Eye of the Storm"
+        height={600}
+        width={600}
+        quality={100}
+        objectFit="contain"
+        priority
+        onMouseMove={function (e) {
+          const mx = e.pageX - x;
+          const my = e.pageY - y;
+          const dx = mx / width;
+          const dy = my / height;
+          const sdfo = (dx * dx) + (dy * dy);
+          if (sdfo <= 0.25) {
+            setHovering(true);
+          } else {
+            setHovering(false);
+          }
+        }}
+        onMouseOut={() => setHovering(false)}
+      />
+    </div>
+  );
+}
