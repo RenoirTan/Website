278b94f729965e6bf9976297d52b554f8fcea1b3
RenoirTan <renoirtan2005@gmail.com>
2024-02-01 20:00:03 +0800

    main: idk

--- app/ui/circus.tsx
+++ app/ui/circus.tsx
@@ -8,11 +8,11 @@ export default function Circus() {
   const [pressed, setPressed] = useState(false);
 
   return (
-    <div className="relative circus bg-l-red w-4/5">
-      <div>
+    <div className="relative circus bg-l-red w-4/5 h-auto">
+      <div className="inset-0 w-full h-full">
         <EyeOfTheStorm pressed={pressed} setPressed={setPressed} />
       </div>
-      <div className="top-0 left-50">
+      <div className="absolute top-0 inset-x-0">
         <Planet show={pressed} />
       </div>
     </div>
--- app/ui/planet.tsx
+++ app/ui/planet.tsx
@@ -3,7 +3,7 @@ import { clsx } from "clsx";
 export default function Planet({ show }: { show?: boolean }) {
   return (
     <div className={clsx(
-      "duration-300 md:duration-500",
+      "duration-300 md:duration-500 text-center",
       show ? "opacity-100" : "opacity-0"
     )}>
       HELLO
