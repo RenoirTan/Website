497fca23972431b929a62292004d48759c12a2cf
RenoirTan <renoirtan2005@gmail.com>
2025-05-31 09:06:07 +0800

    main: ok

--- app/(pages)/about-me/page.tsx
+++ app/(pages)/about-me/page.tsx
@@ -2,5 +2,8 @@ export default function AboutMe() {
   return (<>
     <h1>About Me</h1>
     <p>Hello, my name is Renoir.</p>
+    <p>I'm a programmer who has had at least 6 years of programming by this point. My main languages are Python, Rust, C and recently JavaScript and TypeScript. I have also dabbled in competitions in fields like Cybersecurity and AI, as well as contributed to a few coding events in Singapore.</p>
+    <h2>Programming Languages</h2>
+    <h2>Competitions and Events</h2>
   </>);
 }
