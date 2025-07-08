de9e0094f8616350d0c4af3b9da612f58d803c8d
RenoirTan <renoirtan2005@gmail.com>
2025-07-09 01:18:45 +0800

    main: header size

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,22 +4,22 @@ import Image, { ImageProps } from "next/image";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 className="text-4xl font-bold">{children}</h1>
+      <h1 className="text-3xl md:text-4xl font-bold">{children}</h1>
     ),
     h2: ({ children }) => (
-      <h2 className="text-3xl font-bold">{children}</h2>
+      <h2 className="text-2xl md:text-3xl font-bold">{children}</h2>
     ),
     h3: ({ children }) => (
-      <h3 className="text-2xl font-bold">{children}</h3>
+      <h3 className="text-xl md:text-2xl font-bold">{children}</h3>
     ),
     h4: ({ children }) => (
-      <h4 className="text-xl font-bold">{children}</h4>
+      <h4 className="text-lg md:text-xl font-bold">{children}</h4>
     ),
     h5: ({ children }) => (
-      <h5 className="text-lg font-bold">{children}</h5>
+      <h5 className="text-base md:text-lg font-bold">{children}</h5>
     ),
     h6: ({ children }) => (
-      <h6 className="text-base font-bold">{children}</h6>
+      <h6 className="text-base underline">{children}</h6>
     ),
     img: (props) => (
       <Image className="max-w-full" {...(props as ImageProps)} />
