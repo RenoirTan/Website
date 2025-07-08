90a45e73caeceab3d745d65a43c92a80a95bf2dc
RenoirTan <renoirtan2005@gmail.com>
2025-07-09 01:13:15 +0800

    main: truncate front

--- app/(pages)/[...slug]/pages-layout.tsx
+++ app/(pages)/[...slug]/pages-layout.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-import { BsArrowLeft } from "react-icons/bs";
+import { BsHouseFill } from "react-icons/bs";
 import HorizontalFlyingButton from "../../ui/horizontal-flying-button";
 import { usePathname } from "next/navigation";
 import "../../globals.css";
@@ -18,11 +18,13 @@ export default function PagesLayout({
 
   return <div className="w-full max-h-full flex flex-col items-center">
     <div className="p-1 flex flex-col w-full max-h-full gap-y-3 overflow-hidden">
-      <div className="flex flex-row justify-between">
-        <HorizontalFlyingButton caption="Back" href="/">
-          <BsArrowLeft className="text-xl" />
+      <div className="flex flex-row justify-between gap-5">
+        <HorizontalFlyingButton caption="Home" href="/">
+          <BsHouseFill className="text-xl" />
         </HorizontalFlyingButton>
-        {<h2>{pathname}</h2>}
+        <h2 className="truncate" style={{ direction: "rtl" }}>
+          {pathname}
+        </h2>
       </div>
       <div className="flex flex-col w-full items-center max-h-full gap-y-3 overflow-y-auto pages-layout">
         <div className="flex flex-col gap-3 w-full md:w-[720px] md:p-5">
