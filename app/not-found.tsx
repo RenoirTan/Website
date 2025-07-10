ed719f16b10a6ea6247fd00c9cfd6089dd22541d
RenoirTan <renoirtan2005@gmail.com>
2025-07-10 17:01:14 +0800

    main: back button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,16 @@
-import { BsHouseFill } from "react-icons/bs";
+"use client";
+
+import { BsArrowLeft, BsHouseFill } from "react-icons/bs";
 import { MdCancel } from "react-icons/md";
 import AbsoluteCenter from "./ui/absolute-center";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return <AbsoluteCenter>
     <div className="flex flex-col items-center gap-5">
       <MdCancel
@@ -15,6 +20,12 @@ export default function NotFound() {
       />
       <p>Page not found :(</p>
       <ButtonGroup attached>
+        <Button
+          className="bg-gradient-to-br from-lime-800 to-teal-950 text-white p-3"
+          onClick={() => router.back()}
+        >
+          Back<BsArrowLeft />
+        </Button>
         <Button className="bg-gradient-to-br from-purple-800 to-orange-950 text-white p-3">
           <Link href="/" className="flex flex-row items-center gap-2">
             <BsHouseFill />Home
