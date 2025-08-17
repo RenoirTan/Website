1e75a8cd4bcbed110dec99d4ffe26c4e2d12325b
RenoirTan <renoirtan2005@gmail.com>
2025-08-17 16:48:55 +0800

    main: easing function for caption-image

--- app/ui/caption-image.tsx
+++ app/ui/caption-image.tsx
@@ -58,7 +58,10 @@ export default function CaptionImage(props: ImageProps & {
     initial="hidden"
     whileInView="visible"
     viewport={{ once: true }}
-    transition={{ duration: 0.1 }}
+    transition={{
+      duration: 0.5,
+      ease: "easeInOut",
+    }}
     variants={{
       visible: { opacity: 1, scale: 1 },
       hidden: { opacity: 0, scale: 0.5 },
