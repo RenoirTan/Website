bee28978acc51af70634f0e1c0a37faac7a5f4b7
RenoirTan <renoirtan2005@gmail.com>
2026-06-24 02:47:12 +0800

    flowing: planets orbiting the sun

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@
 import { PageProvider } from "./providers";
 import EyeOfTheStorm from "./ui/eye-of-the-storm";
 import "./globals.css";
-import { motion, useScroll, useTransform } from "motion/react";
-import { useRef } from "react";
+import { easeIn, motion, useScroll, useSpring, useTransform } from "motion/react";
+import { DetailedHTMLProps, HTMLAttributes, useRef, useState } from "react";
 
 export default function Home() {
   return <PageProvider>
@@ -19,7 +19,13 @@ export function NewHomePage() {
     container: containerRef,
     offset: ["start start", "end end"]
   });
-  const scale = useTransform(scrollY, [0, 600], [1, 0.4]);
+  const { eotsScale } = useTransform(scrollY, [0, 600], {
+    eotsScale: [1, 0.4],
+  }, { ease: easeIn });
+  const { planetsOpacity, planetsScale } = useTransform(useSpring(scrollY), [0, 600], {
+    planetsOpacity: [0, 1],
+    planetsScale: ["0%", "100%"],
+  });
 
   const goToEitherEnd = () => {
     const elem = containerRef.current!;
@@ -37,14 +43,45 @@ export function NewHomePage() {
   return (
     <>
       <div ref={containerRef} className="w-full h-full overflow-y-scroll">
-        <div className="w-full h-[500vh]">
-          <div className="sticky top-0 h-[calc(100vh-3rem)] overflow-hidden p-[2rem] flex flex-col items-center">
-            <motion.div style={{ scale }} className="h-full flex flex-row items-center">
-              <EyeOfTheStorm onClick={goToEitherEnd} />
-            </motion.div>
+        <div className="w-full min-h-[calc(100vh+600px)]">
+
+          <div className="sticky top-0 h-[calc(100vh-3rem)] overflow-hidden p-[2rem] w-full flex flex-col items-center">
+            <div className="h-full flex flex-row items-center">
+              <div className="relative">
+                <motion.div style={{ scale: eotsScale }} className="relative z-50">
+                  <EyeOfTheStorm onClick={goToEitherEnd} />
+                </motion.div>
+
+                <motion.div style={{ opacity: planetsOpacity }} className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center transform">
+                  <motion.div style={{ width: planetsScale, height: planetsScale }} className="relative">
+                    <p className="absolute left-0 top-1/2 transform -translate-y-1/2">West</p>
+                    <p className="absolute left-full top-1/2 transform -translate-x-full -translate-y-1/2">East</p>
+                    <p className="absolute left-1/2 top-0 transform -translate-x-1/2">North</p>
+                    <p className="absolute left-1/2 top-full transform -translate-x-1/2 -translate-y-full">South</p>
+                    <p className="absolute left-[14.645%] top-[14.645%] transform -translate-x-[14.645%] -translate-y-[14.645%]">Northwest</p>
+                    <p className="absolute left-[85.355%] top-[14.645%] transform -translate-x-[85.355%] -translate-y-[14.645%]">Northeast</p>
+                    <p className="absolute left-[14.645%] top-[85.355%] transform -translate-x-[14.645%] -translate-y-[85.355%]">Southwest</p>
+                    <p className="absolute left-[85.355%] top-[85.355%] transform -translate-x-[85.355%] -translate-y-[85.355%]">Southeast</p>
+                  </motion.div>
+                </motion.div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </>
   )
+}
+
+function InACircle({
+  degrees,
+  ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
+  degrees: number;
+}) {
+  return (
+    <div
+      {...props}
+    ></div>
+  );
 }
