5d2a26a7dbc92208dbd0dbe039c2081286ff2858
RenoirTan <renoirtan2005@gmail.com>
2026-08-08 17:23:19 +0800

    prannaya: start of something?

--- app/(prannaya)/prannaya/page.tsx
+++ app/(prannaya)/prannaya/page.tsx
@@ -1,5 +1,11 @@
+import { SineIcon } from "@/app/ui/icons/sine";
+
 export default function Prannaya() {
   return <div className="min-h-screen bg-linear-to-b to-[75vh] from-sky-300 to-blue-700">
     <h1>What</h1>
+    <div className="flex flex-row">
+      <SineIcon stroke="white" />
+      <SineIcon stroke="white" />
+    </div>
   </div>;
 }
